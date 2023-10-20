a3cd722b6ffc2f2040d903989085159361360f1b
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-20 18:20:21 +0700

    Setup component

---
+++ src/Navbar.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { links, social } from "./data";
+
+const Navbar = () => {
+  const [showLink, setShowLink] = useState(false);
+
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
