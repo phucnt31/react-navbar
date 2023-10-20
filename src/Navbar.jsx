92217047b99fb5bb87a4fdf5d873818cab0b28f7
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-20 18:26:10 +0700

    Render navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,41 @@
 import React, { useState } from "react";
+import { FaBars } from "react-icons/fa";
 import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
   const [showLink, setShowLink] = useState(false);
 
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" className="logo" />
+          <button
+            type="button"
+            className="nav-toggle"
+            onClick={() => setShowLink(!showLink)}
+          >
+            <FaBars />
+          </button>
+        </div>
+        {showLink && (
+          <div className="links-container">
+            <ul className="links">
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
